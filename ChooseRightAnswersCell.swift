b958328a7ae12de628e4ea24482b7d1d78f74be7
folawer21 <folawer21@mail.ru>
2024-04-26 18:18:58 +0300

    problems

--- ChooseRightAnswersCell.swift
+++ ChooseRightAnswersCell.swift
@@ -5,4 +5,74 @@
 //  Created by Александр  Сухинин on 26.04.2024.
 //
 
-import Foundation
+import UIKit
+
+final class ChooseRightAnswersCell: UITableViewCell{
+    private let taskLabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .black
+        label.backgroundColor = .white
+        return label
+    }()
+    
+    private let checkButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+    
+    func configCell(text: String){
+        taskLabel.text = text
+        
+    }
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configContentView()
+        configButton()
+        addSubViews()
+        applyConstraints()
+        
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configContentView(){
+        contentView.layer.borderWidth = 0.5
+        contentView.layer.cornerRadius = 10
+    }
+    
+ 
+    private func configButton(){
+        checkButton.setImage(UIImage(named: "uncheckedButton"), for: .normal)
+        checkButton.setImage(UIImage(named: "checkedButton"), for: .selected)
+        checkButton.addTarget(self, action: #selector(checkButtonTapped), for: .touchUpInside)
+    }
+    private func addSubViews(){
+        contentView.addSubview(taskLabel)
+        contentView.addSubview(checkButton)
+    }
+    private func applyConstraints(){
+        NSLayoutConstraint.activate([
+            taskLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 6),
+            taskLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 6),
+            taskLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -6),
+            taskLabel.trailingAnchor.constraint(equalTo: checkButton.leadingAnchor, constant: -5),
+            taskLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -6),
+            checkButton.leadingAnchor.constraint(equalTo: taskLabel.trailingAnchor, constant: 5),
+            checkButton.topAnchor.constraint(equalTo: taskLabel.topAnchor),
+            checkButton.bottomAnchor.constraint(equalTo: taskLabel.bottomAnchor),
+            checkButton.widthAnchor.constraint(equalToConstant: 45),
+            checkButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -6)
+        ])
+    }
+    
+    @objc private func checkButtonTapped(){
+        checkButton.isSelected = !checkButton.isSelected
+    }
+}
--- uir/ChooseRightAnswersViewController.swift
+++ uir/ChooseRightAnswersViewController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-final class ChoseRightAnswersViewController: UIViewController{
-    private let inputArr = ["123","13","1413","4143","1535","51351235","53521541","41341","413341","153531315","1345351235","1535135135513"]
+final class ChooseRightAnswersViewController: UIViewController{
+    fileprivate let inputArr = ["123","13","1413","4143","1535","51351235","53521541","41341","413341","153531315","1345351235","1535135135513"]
     private var outputArr: [String]  = []
     private let tableView = {
         let tableView = UITableView()
@@ -17,21 +17,154 @@ final class ChoseRightAnswersViewController: UIViewController{
         tableView.layer.cornerRadius = 10
         return tableView
     }()
+    private let headerView = UIView()
+    private let blockView = UIView()
+    private let givenLabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.textAlignment = .left
+        label.frame = CGRect(x: 0, y: 0, width: label.bounds.size.width, height: label.bounds.size.height)
+        label.font = .systemFont(ofSize: 16)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let headerLabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 16)
+        label.numberOfLines = 0
+        label.text = "Задание"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let sendButton = {
+        let button = UIButton()
+        button.setTitle("Отправить", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font =  .boldSystemFont(ofSize: 16)
+        button.layer.cornerRadius = 20
+        button.layer.borderWidth = 2
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor(named: "blueForHeaders")
+        return button
+    }()
+    @objc private func sendButtonTapped(){
+        let alert = UIAlertController()
+        alert.message = "True" == "False" ? "Верно" : "Ошибка"
+        alert.addAction(UIAlertAction(title: "Закрыть", style: .default, handler: nil))
+        present(alert, animated: true)
+    }
+    private func configViews(){
+        blockView.blockView(radious: 20)
+        headerView.headerView(radious: 20)
+    }
+    private func addTargets(){
+        sendButton.addTarget(self, action: #selector(sendButtonTapped), for: .touchUpInside)
+    }
+    private func addViewsToMain(){
+        view.addSubview(tableView)
+        view.addSubview(headerView)
+        view.addSubview(blockView)
+        view.addSubview(sendButton)
+    }
+    private func addViewsToSubViews(){
+        headerView.addSubview(headerLabel)
+        blockView.addSubview(givenLabel)
+    }
+    private func addSubViews(){
+        addViewsToMain()
+        addViewsToSubViews()
+    }
+    
+    private func applyConstraints(){
+        let blockWidth = UIScreen.main.bounds.width - 32
+        let smallBlockHeight = UIScreen.main.bounds.height / 8
 
+        let headerWidth = blockWidth / 2.5
+        let headerHeight = CGFloat(36)
+        let smallBlockIndent = CGFloat(30)
+        let bigBlockIndent = CGFloat(35)
+        let buttonIndent = bigBlockIndent - 5
+        
+        let buttonWidth = UIScreen.main.bounds.width/2.45
+        let sendButtonHeight = CGFloat(54)
+
+        
+        
+        NSLayoutConstraint.activate([
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            headerView.widthAnchor.constraint(equalToConstant: headerWidth),
+            headerView.heightAnchor.constraint(equalToConstant: headerHeight),
+            
+            blockView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 16),
+            blockView.widthAnchor.constraint(equalToConstant: blockWidth),
+            blockView.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 25),
+            blockView.heightAnchor.constraint(equalToConstant: smallBlockHeight),
+            
+            tableView.topAnchor.constraint(equalTo: blockView.bottomAnchor,constant: smallBlockIndent),
+            tableView.leadingAnchor.constraint(equalTo: blockView.leadingAnchor),
+            tableView.widthAnchor.constraint(equalToConstant: blockWidth),
+            tableView.bottomAnchor.constraint(equalTo: sendButton.topAnchor, constant: -15),
+            
+            sendButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            sendButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            sendButton.heightAnchor.constraint(equalToConstant: sendButtonHeight),
+            sendButton.widthAnchor.constraint(equalToConstant: buttonWidth),
+                
+            
+            headerLabel.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 10),
+            headerLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor,constant: 20),
+            headerLabel.trailingAnchor.constraint(equalTo: headerView.trailingAnchor),
+            headerLabel.bottomAnchor.constraint(equalTo: headerView.bottomAnchor,constant: -14),
+        
+            
+            givenLabel.topAnchor.constraint(equalTo: blockView.topAnchor, constant: 15),
+            givenLabel.leadingAnchor.constraint(equalTo: blockView.leadingAnchor, constant: 20),
+            givenLabel.trailingAnchor.constraint(equalTo: blockView.trailingAnchor, constant: -15)
+
+        
+        
+        ])
+    }
+    
+    init(task:String) {
+        givenLabel.text = task
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        givenLabel.text = ""
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.register(ChooseRightAnswersCell.self, forHeaderFooterViewReuseIdentifier: "rightAnswerCell")
+    
+        configViews()
+        addTargets()
+        addSubViews()
+        applyConstraints()
+//        self.updateLayout(with: self.view.frame.size)
+        
     }
     
 }
 
 
-extension ChoseRightAnswersViewController: UITableViewDataSource{
+extension ChooseRightAnswersViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "rightAnswerCell", for: indexPath) as? ChooseRightAnswersCell else {
+            return UITableViewCell()
+        }
+        let text = inputArr[indexPath.row]
+        cell.configCell(text: text)
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -40,4 +173,7 @@ extension ChoseRightAnswersViewController: UITableViewDataSource{
     
 }
 
+extension ChooseRightAnswersViewController: UITableViewDelegate{
+    
+}
 
